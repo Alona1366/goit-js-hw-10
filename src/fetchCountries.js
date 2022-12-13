f333f4174cfd0bdbbfb4b4c8da2218f0f924adef
Alona Ihnatchenko <olena1366@gmail.com>
2022-12-13 21:47:12 +0200

    update fetchCountries

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,23 +1,11 @@
-import Notiflix from 'notiflix';
-
-const BASE_URL = 'https://restcountries.com/v3.1/name/';
-const filtres = 'name,capital,population,flags,languages'
-
-function notFound() {
-    Notify.failure("Oops, there is no country with that name")
-}
-
-export default function fetchCountries(name) {
-    return fetch (`${BASE_URL}${name}?fields=${filtres}`
-    ).then(r => {
-        if (!r.ok) {
-            notFound();
-            throw Error(statusText);
-        }
-        return r.json()
+function fetchCountries(name) {
+    const url = `https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`;
+    return fetch(url).then(response => {
+      if (!response.ok) {
+        throw new Error(response.status);
+      }
+      return response.json();
     });
-}
-
-// fetchCountries().then(data => console.log(data))
-
-// export default {fetchCountries};
+  }
+  
+  export { fetchCountries };
